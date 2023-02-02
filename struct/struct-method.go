7e9f7b18246323d6b0f72224d8b6bf98705e71e3
Chandra <chandra.febrian@efishery.com>
2023-02-02 23:25:45 +0700

    up
    Signed-off-by: Chandra <chandra.febrian@efishery.com>

--- struct/struct-method.go
+++ struct/struct-method.go
@@ -1,31 +1,31 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-// struct method adalah tipe data seperti tipe data lainnya , dia bisa di gunakan dalam parameter untuk function
-// namun jika ingin menambahkan method(function) ke dalam structs itu bisa, sehingga seakan-akan sebuah struct memiliki function
-// method sama dengan function
+// // struct method adalah tipe data seperti tipe data lainnya , dia bisa di gunakan dalam parameter untuk function
+// // namun jika ingin menambahkan method(function) ke dalam structs itu bisa, sehingga seakan-akan sebuah struct memiliki function
+// // method sama artinya dengan function
 
-// struct dengan nama Customer
-type Customer struct {
-	Name string
-}
+// // struct dengan nama Customer
+// type Customer struct {
+// 	Name string
+// }
 
-// nama var nya harus sama dengan nama struct methodnya (customer Customer)
-// cara baca: func sayhello() dengan paramater struct methode/function (customer Customer)
-func (customer Customer) sayHello() {
-	fmt.Println("hello , my name is", customer.Name)
-}
+// // nama var nya harus sama dengan nama struct methodnya (customer Customer)
+// // cara baca: func sayhello() dengan paramater struct methode/function (customer Customer)
+// func (customer Customer) sayHello() {
+// 	fmt.Println("hello , my name is", customer.Name)
+// }
 
-func (a Customer) FungsiHello() {
-	fmt.Println("halo bro ", a.Name)
-}
+// func (a Customer) FungsiHello() {
+// 	fmt.Println("halo bro ", a.Name)
+// }
 
-func main() {
-	chandra := Customer{Name: "CHANDRA FEBRIAN"}
-	chandra.sayHello()
+// func main() {
+// 	chandra := Customer{Name: "CHANDRA FEBRIAN"}
+// 	chandra.sayHello()
 
-	febrian := Customer{Name: "febrian-devops"}
-	febrian.FungsiHello()
+// 	febrian := Customer{Name: "febrian-devops"}
+// 	febrian.FungsiHello()
 
-}
+// }
--- struct/struct.go
+++ struct/struct.go
@@ -1,24 +1,37 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// // keyword struct adalah template data sama dengan class kalau di dart OOP ,
-// // dengan menuliskan type NamaStructnya struct
-// type Customer struct {
-// 	Nama, Alamat string
-// 	Age          int
-// }
+// keyword struct adalah template data sama dengan class kalau di dart OOP ,
+// dengan menuliskan type NamaStructnya struct
+type Customer struct {
+	Nama, Alamat string
+	Age          int
+}
 
-// // cara memanggil struct dengan membuat variable baru lalu panggil nama Struct nya
-// func main() {
-// 	chandra := Customer{
-// 		Age:    30,
-// 		Alamat: "medan",
-// 		Nama:   "chandra febrian",
-// 	}
+type Human struct {
+	orang string
+	umur  int
+}
 
-// 	fmt.Println(chandra)
-// 	fmt.Println(chandra.Nama)
-// 	fmt.Println(chandra.Alamat)
-// 	fmt.Println(chandra.Age)
-// }
+// cara memanggil struct dengan membuat variable baru lalu panggil nama Struct nya
+func main() {
+	chandra := Customer{
+		Age:    30,
+		Alamat: "medan",
+		Nama:   "chandra febrian",
+	}
+
+	manusia := Human{
+		orang: "chandra nih bos",
+		umur:  33,
+	}
+
+	fmt.Println(chandra)
+	fmt.Println(chandra.Nama)
+	fmt.Println(chandra.Alamat)
+	fmt.Println(chandra.Age)
+	fmt.Println("halo : ", manusia)
+	fmt.Println(manusia.orang)
+	fmt.Println(manusia.umur)
+}
