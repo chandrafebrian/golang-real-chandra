105e8cd51153dcb75e435fc2611b2904b76638ad
Chandra <chandra.febrian@efishery.com>
2023-02-03 12:05:48 +0700

    up
    Signed-off-by: Chandra <chandra.febrian@efishery.com>

--- interface/interface.go
+++ interface/interface.go
@@ -4,17 +4,27 @@ import "fmt"
 
 // // interface adalah tipe data abstract,dia tidak memiliki implementasi langsung
 // // interface adalah contract func , jika ingin menggunakan nya harus memakai fungsi dari contract interface tersebut Getname() tipe kembalian string
-// // cara baca : membuat interface kontrak dengan nama hasname dan didalam nya punya method func dengan nama Getname() yg wajib di sebutkan saat di panggil
+// // cara baca : membuat interface kontrak dengan nama Hasname dan didalam nya ada method func dengan nama Getname() yg wajib di sebutkan saat memanggil interface name
 
 type Hasname interface {
 	Getname() string
 }
 
 // // func Sayhello (dengan parameter var hasname dan type interface Hasname)
-// // saat mendefind string wajib memanggil var type interface dan method nya
+// // saat mendefind(menjelaskan) string wajib memanggil var type interface dan method(function) nya
 func SayHello(hasname Hasname) {
 	fmt.Println("hello ", hasname.Getname())
 
+}
+
+// --------------------------------------------------------
+
+type Engineer struct {
+	Develepor string
+}
+
+func (engineer Engineer) Getname() string {
+	return engineer.Develepor
 }
 
 // type struct sama dengan class Person di dart OOP
@@ -26,6 +36,8 @@ func (person Person) Getname() string {
 	return person.Name
 }
 
+// -------------------------------------------------------
+
 type Animal struct {
 	Jenis string
 }
@@ -33,6 +45,8 @@ type Animal struct {
 func (hewan Animal) Getname() string {
 	return hewan.Jenis
 }
+
+// -------------------------------------------------------
 
 func main() {
 	judul := Person{
@@ -45,4 +59,8 @@ func main() {
 	}
 	SayHello(cat)
 
+	orangDev := Engineer{
+		Develepor: "chandra best of the best",
+	}
+	SayHello(orangDev)
 }
--- struct/struct.go
+++ struct/struct.go
@@ -1,37 +1,37 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-// keyword struct adalah template data sama dengan class kalau di dart OOP ,
-// dengan menuliskan type NamaStructnya struct
-type Customer struct {
-	Nama, Alamat string
-	Age          int
-}
+// // keyword struct adalah template data sama dengan class kalau di dart OOP ,
+// // dengan menuliskan type NamaStructnya struct
+// type Customer struct {
+// 	Nama, Alamat string
+// 	Age          int
+// }
 
-type Human struct {
-	orang string
-	umur  int
-}
+// type Human struct {
+// 	orang string
+// 	umur  int
+// }
 
-// cara memanggil struct dengan membuat variable baru lalu panggil nama Struct nya
-func main() {
-	chandra := Customer{
-		Age:    30,
-		Alamat: "medan",
-		Nama:   "chandra febrian",
-	}
+// // cara memanggil struct dengan membuat variable baru lalu panggil nama Struct nya
+// func main() {
+// 	chandra := Customer{
+// 		Age:    30,
+// 		Alamat: "medan",
+// 		Nama:   "chandra febrian",
+// 	}
 
-	manusia := Human{
-		orang: "chandra nih bos",
-		umur:  33,
-	}
+// 	manusia := Human{
+// 		orang: "chandra nih bos",
+// 		umur:  33,
+// 	}
 
-	fmt.Println(chandra)
-	fmt.Println(chandra.Nama)
-	fmt.Println(chandra.Alamat)
-	fmt.Println(chandra.Age)
-	fmt.Println("halo : ", manusia)
-	fmt.Println(manusia.orang)
-	fmt.Println(manusia.umur)
-}
+// 	fmt.Println(chandra)
+// 	fmt.Println(chandra.Nama)
+// 	fmt.Println(chandra.Alamat)
+// 	fmt.Println(chandra.Age)
+// 	fmt.Println("halo : ", manusia)
+// 	fmt.Println(manusia.orang)
+// 	fmt.Println(manusia.umur)
+// }
