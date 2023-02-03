89284a3ec0052f8ff65f5f33798de4b8ba25008d
Chandra <chandra.febrian@efishery.com>
2023-02-03 13:13:23 +0700

    up
    Signed-off-by: Chandra <chandra.febrian@efishery.com>

--- pointer/pointer-func.go
+++ pointer/pointer-func.go
@@ -1,20 +1,20 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-type Adress struct {
-	City, Province, Country string
-}
+// type Adress struct {
+// 	City, Province, Country string
+// }
 
-// tanda bintang akan menuju reference ke *Adress struct agar bisa berubah country field nya dengan yg baru
-func ChangeAddress(newAddress *Adress) {
-	newAddress.Country = "amerika"
+// // tanda bintang akan menuju reference ke *Adress struct agar bisa berubah country field nya dengan yg baru
+// func ChangeAddress(newAddress *Adress) {
+// 	newAddress.Country = "amerika"
 
-}
+// }
 
-// lalu memanggil func ChangeAddress(dengan tanda dan &oldAddress) sebagai penunjuk perubahan data nya di kirim ke var tersebut
-func main() {
-	oldAddress := Adress{"subang", "jawabarat", "indonesia"}
-	ChangeAddress(&oldAddress)
-	fmt.Println(oldAddress)
-}
+// // lalu memanggil func ChangeAddress(dengan tanda dan &oldAddress) sebagai penunjuk perubahan data nya di kirim ke var tersebut
+// func main() {
+// 	oldAddress := Adress{"subang", "jawabarat", "indonesia"}
+// 	ChangeAddress(&oldAddress)
+// 	fmt.Println(oldAddress)
+// }
--- pointer/pointer.go
+++ pointer/pointer.go
@@ -1,24 +1,27 @@
 package main
 
-// keyword pointer adalah kemampuan membuat reference ke lokasi data di memory yang sama, tanpa duplikasi data yang sudah ada
+import "fmt"
+
+// keyword pointer adalah kemampuan membuat reference(tujuan) ke lokasi data di memory yang sama, tanpa duplikasi data yang sudah ada
 // sederhana nya dengan pointer bisa membuat passing data by reference
 // keyword pointer menggunakan operator "&" akan membuat memory baru dengan mengcopy data addrs1 dan merubah value nya sesuai data yg baru untuk addrs2
 // address2.city , merubah nama kota nya dari addres1
-// type Adress struct {
-// 	city, province, country string
-// }
 
-// func main() {
+type Adress struct {
+	city, province, country string
+}
 
-// 	address1 := Adress{"bandung", "jabar", "indon"}
-// 	address2 := &address1
-// 	// address2.city = "jaktim"
+func main() {
 
-// 	// keyword bintang * akan memaksa value yg di refer(dituju) menggunakan keyword bintang *address2 = address("update1","up2","up3")
-// 	// penggunaan keyword nya harus sepasang dengan * dan &
-// 	*address2 = Adress{"solo", "jateng", "indon"}
+	address1 := Adress{"bandung", "jabar", "indon"}
+	address2 := &address1
+	// address2.city = "jaktim"
 
-// 	fmt.Println("add1 :", address1)
-// 	fmt.Println("add2 :", address2)
+	// keyword bintang * akan memaksa value yg di refer(dituju) menggunakan keyword bintang *address2 = address("update1","up2","up3")
+	// penggunaan keyword nya harus sepasang dengan * dan &
+	*address2 = Adress{"solo", "jateng", "indon"}
 
-// }
+	fmt.Println("add1 :", address1)
+	fmt.Println("add2 :", address2)
+
+}
