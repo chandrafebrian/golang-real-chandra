0c925c179534e09856ad921815e029c27d1bd03c
Chandra <chandra.febrian@efishery.com>
2023-02-05 20:52:37 +0700

    up
    Signed-off-by: Chandra <chandra.febrian@efishery.com>

--- interface/interface-kosong.go
+++ interface/interface-kosong.go
@@ -1,23 +1,23 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-// interface kosong adalah interface yang tidak memiliki deklarasi method function, hal ini
-// membuat semua tipe data yang di masukan akan otomatis menjadi implementasinya
+// // interface kosong adalah interface yang tidak memiliki deklarasi method function, hal ini
+// // membuat semua tipe data yang di masukan akan otomatis menjadi implementasinya
 
-func Ups(i int) interface{} {
-	if i == 1 {
-		return 1
-	} else if i == 2 {
-		return true
-	} else {
-		return "Ups Bro tidak masuk dalam list int"
-	}
-}
+// func Ups(i int) interface{} {
+// 	if i == 1 {
+// 		return 1
+// 	} else if i == 2 {
+// 		return true
+// 	} else {
+// 		return "Ups Bro tidak masuk dalam list int"
+// 	}
+// }
 
-func main() {
+// func main() {
 
-	var test interface{} = Ups(1)
-	fmt.Println(test)
+// 	var test interface{} = Ups(1)
+// 	fmt.Println(test)
 
-}
+// }
--- interface/interface.go
+++ interface/interface.go
@@ -1,66 +1,66 @@
 package main
 
-// import "fmt"
+import "fmt"
 
-// // // interface adalah tipe data abstract,dia tidak memiliki implementasi langsung
-// // // interface adalah contract func , jika ingin menggunakan nya harus memakai fungsi dari contract interface tersebut Getname() tipe kembalian string
-// // // cara baca : membuat interface kontrak dengan nama Hasname dan didalam nya ada method func dengan nama Getname() yg wajib di sebutkan saat memanggil interface name
+// // interface adalah tipe data abstract,dia tidak memiliki implementasi langsung
+// // interface adalah contract func , jika ingin menggunakan nya harus memakai fungsi dari contract interface tersebut Getname() tipe kembalian string
+// // cara baca : membuat interface kontrak dengan nama Hasname dan didalam nya ada method func dengan nama Getname() yg wajib di sebutkan saat memanggil Hasname
 
-// type Hasname interface {
-// 	Getname() string
-// }
+type Hasname interface {
+	Getname() string
+}
 
-// // // func Sayhello (dengan parameter var hasname dan type interface Hasname)
-// // // saat mendefind(menjelaskan) string wajib memanggil var type interface dan method(function) nya
-// func SayHello(hasname Hasname) {
-// 	fmt.Println("hello ", hasname.Getname())
+// // func Sayhello (dengan parameter var hasname dan type interface Hasname)
+// // saat mendefind(menjelaskan) string wajib memanggil var type interface dan method(function) nya
+func SayHello(hasname Hasname) {
+	fmt.Println("hello ", hasname.Getname())
 
-// }
+}
 
-// // --------------------------------------------------------
+// --------------------------------------------------------
 
-// type Engineer struct {
-// 	Develepor string
-// }
+type Engineer struct {
+	Develepor string
+}
 
-// func (engineer Engineer) Getname() string {
-// 	return engineer.Develepor
-// }
+func (engineer Engineer) Getname() string {
+	return engineer.Develepor
+}
 
-// // type struct sama dengan class Person di dart OOP
-// type Person struct {
-// 	Name string
-// }
+// type struct sama dengan class Person di dart OOP
+type Person struct {
+	Name string
+}
 
-// func (person Person) Getname() string {
-// 	return person.Name
-// }
+func (person Person) Getname() string {
+	return person.Name
+}
 
-// // -------------------------------------------------------
+// -------------------------------------------------------
 
-// type Animal struct {
-// 	Jenis string
-// }
+type Animal struct {
+	Jenis string
+}
 
-// func (hewan Animal) Getname() string {
-// 	return hewan.Jenis
-// }
+func (hewan Animal) Getname() string {
+	return hewan.Jenis
+}
 
-// // -------------------------------------------------------
+// -------------------------------------------------------
 
-// func main() {
-// 	judul := Person{
-// 		Name: "chandra",
-// 	}
-// 	SayHello(judul)
+func main() {
+	judul := Person{
+		Name: "chandra",
+	}
+	SayHello(judul)
 
-// 	cat := Animal{
-// 		Jenis: "anggora",
-// 	}
-// 	SayHello(cat)
+	cat := Animal{
+		Jenis: "anggora",
+	}
+	SayHello(cat)
 
-// 	orangDev := Engineer{
-// 		Develepor: "chandra best of the best",
-// 	}
-// 	SayHello(orangDev)
-// }
+	orangDev := Engineer{
+		Develepor: "chandra best of the best",
+	}
+	SayHello(orangDev)
+}
